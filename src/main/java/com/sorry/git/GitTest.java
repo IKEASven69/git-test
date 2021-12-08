19f01c3bc33a94293bfb2719224ad1b856515670
sorry <sorry@gmail.com>
2021-12-08 16:03:20 +0800

    push commit

--- src/main/java/com/sorry/git/GitTest.java
+++ src/main/java/com/sorry/git/GitTest.java
@@ -8,5 +8,7 @@ public class GitTest {
         System.out.println("hello gayhub");
         System.out.println("master");
         System.out.println("hot-fix");
+        System.out.println("push-test");
+
     }
 }
